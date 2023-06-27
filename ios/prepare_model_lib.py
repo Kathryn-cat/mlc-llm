6c648803c247a1c1932f40db6e4a651af6de45b6
Jinqi Chen <jinqic@alumni.cmu.edu>
2023-06-27 23:30:05 +0800

    modify app-config.json

--- ios/prepare_model_lib.py
+++ ios/prepare_model_lib.py
@@ -2,17 +2,18 @@ import json
 import os
 from tvm.contrib import cc
 
+
 def main():
     app_config = json.load(open("MLCChat/app-config.json", "r"))
     target = "iphone"
-    artifact_path = os.path.abspath(os.path.join("..", "dist"))
+    artifact_path = os.path.abspath(os.path.join("../..", "dist"))
 
     tar_list = []
 
     for local_id in app_config["model_libs"]:
         paths = [
             os.path.join(artifact_path, local_id, f"{local_id}-{target}.tar"),
-            os.path.join(artifact_path, "prebuilt", "lib", f"{local_id}-{target}.tar")
+            os.path.join(artifact_path, "prebuilt", "lib", f"{local_id}-{target}.tar"),
         ]
         valid_paths = [p for p in paths if os.path.isfile(p)]
         if not valid_paths:
